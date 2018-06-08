4e94b42ef6f99150a2999f54ef4e2bd7a8d326ac
Caua539 <caua539@live.com>
2018-06-08 03:40:24 +0000

    Adicionando arquivos principais

---
+++ start.sh
@@ -0,0 +1,10 @@
+
+#!/bin/bash
+echo "Criando volume REPORTS"
+docker volume create REPORTS
+
+echo "copiando o relatório modelo para a pasta do volume criado"
+cp /reports/Customers.rptdesign /var/lib/docker/volumes/REPORTS/_data
+
+echo "Criando container"
+docker run --name BIRT001 -i -t -p 9090:8080 -v reports:/usr/local/tomcat/webapps/birt/reports cdorde/mybirt:4.5.0

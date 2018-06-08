f6e838175344e2dd363e746bf63be0cf7f7664ce
Cauã M. Pessoa <caua539@live.com>
2018-06-08 00:42:18 -0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -4,7 +4,7 @@ echo "Criando volume REPORTS"
 docker volume create REPORTS
 
 echo "copiando o relatório modelo para a pasta do volume criado"
-cp /reports/Customers.rptdesign /var/lib/docker/volumes/REPORTS/_data
+cp reports/Customers.rptdesign /var/lib/docker/volumes/REPORTS/_data
 
 echo "Criando container"
 docker run --name BIRT001 -i -t -p 9090:8080 -v reports:/usr/local/tomcat/webapps/birt/reports cdorde/mybirt:4.5.0
